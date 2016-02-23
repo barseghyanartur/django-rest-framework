b8dd34192d5f520579b6223df21d484f070eafea
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2016-02-23 16:37:05 +0100

    make it possible to add a custom message to permission check

--- rest_framework/views.py
+++ rest_framework/views.py
@@ -315,9 +315,13 @@ class APIView(View):
         Raises an appropriate exception if the request is not permitted.
         """
         for permission in self.get_permissions():
-            if not permission.has_permission(request, self):
+            has_perm = permission.has_permission(request, self)
+            if not has_perm:
+                message = has_perm.message \
+                    if hasattr(has_perm, 'message') and has_perm.message \
+                    else getattr(permission, 'message', None)
                 self.permission_denied(
-                    request, message=getattr(permission, 'message', None)
+                    request, message=message
                 )
 
     def check_object_permissions(self, request, obj):
@@ -326,9 +330,13 @@ class APIView(View):
         Raises an appropriate exception if the request is not permitted.
         """
         for permission in self.get_permissions():
-            if not permission.has_object_permission(request, self, obj):
+            has_perm = permission.has_object_permission(request, self, obj)
+            if not has_perm:
+                message = has_perm.message \
+                    if hasattr(has_perm, 'message') and has_perm.message \
+                    else getattr(permission, 'message', None)
                 self.permission_denied(
-                    request, message=getattr(permission, 'message', None)
+                    request, message=message
                 )
 
     def check_throttles(self, request):
